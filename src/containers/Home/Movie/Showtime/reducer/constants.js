b635d59428f9dde7ab3535400cc28f6239dd2a7e
The Nguyen <nthenguyeen@gmail.com>
2022-07-31 20:27:46 +0700

    show time

---
+++ src/containers/Home/Movie/Showtime/reducer/constants.js
@@ -0,0 +1,13 @@
+export const CUM_RAP_REQUEST = "@/cumRapReducer/CUM_RAP_REQUEST";
+export const CUM_RAP_SUCCESS = "@/cumRapReducer/CUM_RAP_SUCCESS";
+export const CUM_RAP_FAILED = "@/cumRapReducer/CUM_RAP_FAILED";
+
+export const HE_THONG_RAP_REQUEST = "@/heThongRapReducer/HE_THONG_RAP_REQUEST";
+export const HE_THONG_RAP_SUCCESS = "@/heThongRapReducer/HE_THONG_RAP_SUCCESS";
+export const HE_THONG_RAP_FAILED = "@/heThongRapReducer/HE_THONG_RAP_FAILED";
+
+export const TAO_LICH_CHIEU_REQUEST =
+  "@/lichChieuReducer/TAO_LICH_CHIEU_REQUEST";
+export const TAO_LICH_CHIEU_SUCCESS =
+  "@/lichChieuReducer/TAO_LICH_CHIEU_SUCCESS";
+export const TAO_LICH_CHIEU_FAILED = "@/lichChieuReducer/TAO_LICH_CHIEU_FAILED";
